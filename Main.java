5378a93c0b1e2fd26f09e0565c1af17961cc07ba
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-23 11:35:16 +0530

    identifiers

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Good identifier
+        int minutesPerHour = 60;
+        System.out.println(minutesPerHour);
+
+    }
+}
