8e43eccee9e1a9087980e7094e6e365778b83d31
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-23 13:56:02 +0530

    calling methods class variables

--- Kamal.java
+++ Kamal.java
@@ -13,9 +13,12 @@ public class Kamal implements Student{
     public void walk() {
         System.out.println("walk");
     }
+    int num1 =330;
     public static void main(String[] args) {
         Student x = new Kamal();
+        Kamal y = new Kamal();//for num1
+        System.out.println(y.num1);
+        y.eat();
         System.out.println(Student.num);
-
     }
 }
