27800bd10695e1809aace823e012703e5bfb2ebb
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-23 11:37:06 +0530

    multiple variables

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main {
         // Good identifier
         int minutesPerHour = 60;
         System.out.println(minutesPerHour);
-
+        //multiple variables
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
     }
 }
