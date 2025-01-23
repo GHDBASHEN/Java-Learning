8eb9250a08d7dc77d856bea1ee4af8cc49d8b7ff
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-23 11:54:56 +0530

    Data types

--- Main.java
+++ Main.java
@@ -11,5 +11,15 @@ public class Main {
         a = b = c =50;
         System.out.println(a + b + c);
 
+        int myNum = 5;               // integer (whole number)
+        float myFloatNum = 5.99f;    // floating point number
+        char myLetter = 'D';         // character
+        boolean myBool = true;       // boolean
+        String myText = "Hello";     // String
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
     }
 }
