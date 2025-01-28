35069116085c1827cefd148a0c1ae6d53d1d03c0
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-28 16:01:04 +0530

    practical c.ii

---
+++ MyArrayDemo.java
@@ -0,0 +1,9 @@
+public class MyArrayDemo {
+    public static void main(String[] args) {
+        int sum = 0, x=5,y=0;
+        for(;x>y;x --,y++){
+            sum +=(x+y);
+        }
+        System.out.println(sum);
+    }
+}
